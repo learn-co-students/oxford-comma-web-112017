84a52bb5736a85db191730cb362467b6400747ae
David Brennan <ddbren@gmail.com>
2017-11-05 18:48:45 -0500

    Oxford Comma function working

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  l = array.size
+  if l == 1
+    return array.join
+  elsif l == 2
+    return array.join(" and ")
+  else
+    lastitem = array.pop
+    return array.join(", ") + ", and #{lastitem}"
+  end
 end
